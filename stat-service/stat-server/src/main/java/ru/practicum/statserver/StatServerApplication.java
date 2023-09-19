ea0f98560074e9ab85c030e660a9a7ff2dfcb2ba
Kirill <reskovkirill@yandex.ru>
2023-09-19 11:55:52 +0300

    fix pipeline

--- stat-service/stat-server/src/main/java/ru/practicum/statserver/StatServerApplication.java
+++ stat-service/stat-server/src/main/java/ru/practicum/statserver/StatServerApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StatServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatServerApplication.class, args);
+
     }
 }
