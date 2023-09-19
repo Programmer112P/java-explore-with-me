d70918bd126922bacd37dd9450bd238012c7d334
Kirill <reskovkirill@yandex.ru>
2023-09-19 12:21:02 +0300

    fix pipeline (4)

--- stat-service/stat-dto/src/main/java/ru/practicum/statdto/ViewStatsDto.java
+++ stat-service/stat-dto/src/main/java/ru/practicum/statdto/ViewStatsDto.java
@@ -1,10 +1,30 @@
 package ru.practicum.statdto;
 
-public interface ViewStatsDto {
+import lombok.*;
 
-    String getApp();
+import java.util.Objects;
 
-    String getUri();
+@Getter
+@Setter
+@AllArgsConstructor
+public class ViewStatsDto {
 
-    Integer getHits();
+    private String app;
+
+    private String uri;
+
+    private Integer hits;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ViewStatsDto that = (ViewStatsDto) o;
+        return Objects.equals(app, that.app) && Objects.equals(uri, that.uri) && Objects.equals(hits, that.hits);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(app, uri, hits);
+    }
 }
